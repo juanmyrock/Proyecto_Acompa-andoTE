4df03c64840421fa05060dc36246347c4bd43216
Juanmyrock <juanmyrock@gmail.com>
2025-06-01 14:34:19 -0300

    ultimo insert

---
+++ CapaDTO/cls_SesionActivaDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CapaDTO
+{
+    public class cls_SesionActivaDTO
+    {
+        public int UsuarioId { get; set; }
+        public string Token { get; set; }
+        public string IP { get; set; }
+        public DateTime FechaInicio { get; set; }
+    }
+
+}
