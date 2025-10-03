2f577da9ea43de3071c134bb4aeef0d7e6457fc6
S384xX <sebastiangonflo@gmail.com>
2025-10-03 19:48:30 -0300

    Nueva rama pa poder probar todo antes de implementarlo jeje

--- CapaDTO/SistemaDTO/cls_Tramite_PacienteDTO.cs
+++ CapaDTO/SistemaDTO/cls_Tramite_PacienteDTO.cs
@@ -8,13 +8,13 @@ namespace CapaDTO.SistemaDTO
 {
     public class cls_Tramite_PacienteDTO
     {
-        public int id_tramite { get; set; }
+        public int id_tp { get; set; }
         public int id_paciente { get; set; }
-        public string Descripcion { get; set; } 
-        public string EstadoActual { get; set; } 
+        public int id_tramite { get; set; }
+        public string Descripcion { get; set; }
+        public string EstadoActual { get; set; }
         public string NombrePacienteCompleto { get; set; }
-        public int id_tp {  get; set; }
-        public DateTime fecha_hora { get; set; }
-
+        public DateTime fecha_creacion { get; set; }
+        public string comentario_inicial { get; set; }
     }
 }
