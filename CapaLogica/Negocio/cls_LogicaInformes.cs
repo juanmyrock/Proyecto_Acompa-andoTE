76d298d5480f8beb626ecf426d6d56db929a0e0d
S384xX <sebastiangonflo@gmail.com>
2025-10-23 21:28:37 -0300

    Va la base de nuevo + cls_LogicaInformes (vacia todavia)

---
+++ CapaLogica/Negocio/cls_LogicaInformes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica.Negocio
+{
+    internal class cls_LogicaInformes
+    {
+    }
+}
--- CapaVistas/Forms Menu/frmInformesAT.cs
+++ CapaVistas/Forms Menu/frmInformesAT.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDTO.SistemaDTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,43 @@ namespace CapaVistas.Forms_Menu
         public frmInformesAT()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtBusquedaPaciente.Text, out int dniBuscado))
+            {
+                MessageBox.Show("Por favor, ingrese un número de DNI válido.", "Error de entrada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBusquedaPaciente.Clear();
+                return;
+            }
+
+            try
+            {
+                cls_PacienteDTO pacienteEncontrado = _logicaPaciente.BuscarPacientePorDni(dniBuscado);
+
+                if (pacienteEncontrado != null)
+                {
+
+                    List<cls_PacienteDTO> resultado = new List<cls_PacienteDTO> { pacienteEncontrado };
+
+                    dgvVerPacientes.DataSource = resultado;
+
+                    MessageBox.Show($"Paciente {pacienteEncontrado.Nombre} {pacienteEncontrado.Apellido} encontrado.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+
+                    MessageBox.Show($"No se encontró ningún paciente con DNI: {dniBuscado}.", "No Encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CargarPacientesEnDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al buscar el paciente: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                CargarPacientesEnDataGridView();
+            }
         }
     }
 }
