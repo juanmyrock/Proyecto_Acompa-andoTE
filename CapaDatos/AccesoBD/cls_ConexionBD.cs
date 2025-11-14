a8799b2dea3bf08282138cda6812074904e34dcc
S384xX <sebastiangonflo@gmail.com>
2025-11-14 04:44:23 -0300

    Pusheo con la conexión a BD automática

--- CapaDatos/AccesoBD/cls_ConexionBD.cs
+++ CapaDatos/AccesoBD/cls_ConexionBD.cs
@@ -1,23 +1,121 @@
-﻿using System.Data.SqlClient;
+﻿//using System.Data.SqlClient;
+
+//namespace CapaDatos
+//{
+//    public abstract class cls_ConexionBD
+//    {
+//        private readonly string conexion;
+//        public cls_ConexionBD()
+//        {   //notebook seba
+//            //conexion = @"Server=SEBASTIANGONFLO; Database=ProyectoAT; User Id=SEBASTIANGONFLO\sebas; Integrated Security=True;";
+
+//            //principal seba
+//            conexion = @"Server=DESKTOP-BS1413A; Database=ProyectoAT; User Id=DESKTOP-BS1413A\Sebas; Integrated Security=True;";
+
+//        }
+
+//        protected SqlConnection GetConexion() 
+//        {
+//            return new SqlConnection(conexion);
+//        }
+
+//    }
+//}
+using System;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     public abstract class cls_ConexionBD
     {
         private readonly string conexion;
-        public cls_ConexionBD()
-        {   //notebook seba
-            //conexion = @"Server=SEBASTIANGONFLO; Database=ProyectoAT; User Id=SEBASTIANGONFLO\sebas; Integrated Security=True;";
 
-            //principal seba
-            conexion = @"Server=DESKTOP-BS1413A; Database=ProyectoAT; User Id=DESKTOP-BS1413A\Sebas; Integrated Security=True;";
-            
+        public cls_ConexionBD()
+        {
+            conexion = ObtenerCadenaConexionAutomatica();
         }
 
-        protected SqlConnection GetConexion() 
+        protected SqlConnection GetConexion()
         {
             return new SqlConnection(conexion);
         }
 
+        private string ObtenerCadenaConexionAutomatica()
+        {
+            string[] servidoresProbables = {
+                ".",                          
+                "localhost",                 
+                "(local)",                   
+                @".\SQLEXPRESS",            
+                "localhost\\SQLEXPRESS"      
+            };
+
+            string baseDeDatos = "ProyectoAT";
+
+            foreach (string servidor in servidoresProbables)
+            {
+                string cadenaPrueba = $"Server={servidor}; Database={baseDeDatos}; Integrated Security=True;";
+
+                if (ProbarConexion(cadenaPrueba))
+                {
+                    return cadenaPrueba;
+                }
+            }
+
+            string instanciaEncontrada = BuscarInstanciasSQL();
+            if (!string.IsNullOrEmpty(instanciaEncontrada))
+            {
+                return $"Server={instanciaEncontrada}; Database={baseDeDatos}; Integrated Security=True;";
+            }
+
+            throw new Exception("No se pudo encontrar una instancia de SQL Server disponible.");
+        }
+
+        private bool ProbarConexion(string cadenaConexion)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string BuscarInstanciasSQL()
+        {
+            try
+            {
+                DataTable instancias = SqlDataSourceEnumerator.Instance.GetDataSources();
+
+                foreach (DataRow row in instancias.Rows)
+                {
+                    string nombreServidor = row["ServerName"].ToString();
+                    string nombreInstancia = row["InstanceName"].ToString();
+
+                    if (string.IsNullOrEmpty(nombreInstancia))
+                    {
+
+                        return nombreServidor;
+                    }
+                    else
+                    {
+                        return $"{nombreServidor}\\{nombreInstancia}";
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
     }
 }
