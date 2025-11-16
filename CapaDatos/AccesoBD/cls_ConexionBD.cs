6ef62ecd225c348183c25b46fa8aa95efc51eafe
S384xX <sebastiangonflo@gmail.com>
2025-11-16 06:47:58 -0300

    Se pushea con el instalador funcionando bien (aparentemente)

--- CapaDatos/AccesoBD/cls_ConexionBD.cs
+++ CapaDatos/AccesoBD/cls_ConexionBD.cs
@@ -1,27 +1,4 @@
-﻿//using System.Data.SqlClient;
-
-//namespace CapaDatos
-//{
-//    public abstract class cls_ConexionBD
-//    {
-//        private readonly string conexion;
-//        public cls_ConexionBD()
-//        {   //notebook seba
-//            //conexion = @"Server=SEBASTIANGONFLO; Database=ProyectoAT; User Id=SEBASTIANGONFLO\sebas; Integrated Security=True;";
-
-//            //principal seba
-//            conexion = @"Server=DESKTOP-BS1413A; Database=ProyectoAT; User Id=DESKTOP-BS1413A\Sebas; Integrated Security=True;";
-
-//        }
-
-//        protected SqlConnection GetConexion() 
-//        {
-//            return new SqlConnection(conexion);
-//        }
-
-//    }
-//}
-using System;
+﻿using System;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
